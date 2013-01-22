f26170a3da0fb025053414343f3a3517b79792e2
Matthaus Owens <matthaus@puppetlabs.com>
2013-01-21 23:25:59 -0800

    Progress on moving to xpath.

--- bot/new_bot.rb
+++ bot/new_bot.rb
@@ -1,36 +1,46 @@
-location = doc.xpath('//xmlns:location').attribute("desc").value
-if route
-  arrivals = doc.xpath("//xmlns:arrivals[@route=#{route}]")
-else
-  arrivals = doc.xpath('//xmlns:arrivals')
+def parse_results(doc, route)
+  unless (t_loc = doc.xpath('//xmlns:location')).empty?
+    location = t_loc.attribute("desc").value
+  end
+
+  if route
+    arrivals = doc.xpath("//xmlns:arrival[@route=#{route}]")
+  else
+    arrivals = doc.xpath('//xmlns:arrival')
+  end
+
+  unless (t_error = doc.xpath('//xmlns:errorMessage')).empty?
+    error = t_error.inner_text
+  end
+
+  arrivals.each do |node|
+    puts node.name
+    if node.attributes.include?("estimated")
+      estimated = node.attribute("estimated").value.to_i/1000
+      estimatedDiff = estimated - Time.now.to_i
+      estimate = parse_time(estimated)
+
+      if estimate[:hours].zero?
+        arrival = "#{estimate[:minutes]} minutes, #{estimate[:seconds]} seconds\n"
+      else
+        arrival = "#{estimate[:hours]} hour(s), #{estimate[:minutes]} minutes, #{estimate[:seconds]} seconds\n"
+      end
+    end
+
+    if node.attributes.include?("scheduled")
+      scheduled = node.attribute("scheduled").value.to_i/1000
+      arrival = "<no estimate available>"
+    end
+
+    # results  "Stop ID: #{stopID} - #{node.attribute("desc")}\n"
+    # results[block] = "#{node.attribute("shortSign")} - Estimated Arrival: #{arriveString} (Scheduled at #{scheduled.strftime("%H:%M")})\n"
+  end
 end
 
-doc.each do | node |
-  case node.name
-    when "location"
-      if (node.depth == 1)
-        returnString << "Stop ID: #{stopID} - #{node.attribute("desc")}\n"
-      end
-    when "arrival"
-      count += 1
-      if (lineNum == nil or node.attribute("route") == lineNum)
-        block = node.attribute("block")
-        curTime = Time.now.to_i
-        arrival = node.attribute("estimated")
-        arrival = Time.at(arrival[0..arrival.length-4].to_i)
-        arriveDiff = (arrival.to_i - curTime)
-        if (node.attribute("status") == "estimated")
-          if (arriveDiff > 3600)
-            arriveString = "#{arriveDiff/3600} hour(s), #{(arriveDiff%3600)/60} minutes, #{(arriveDiff%3600)%60} seconds"
-          else
-            arriveString = "#{arriveDiff/60} minutes, #{arriveDiff%60} seconds"
-          end
-        else
-          arriveString = "<no estimate available>"
-        end
-        scheduled = node.attribute("scheduled")
-        scheduled = Time.at(scheduled[0..scheduled.length-4].to_i)
-        results[block] = "#{node.attribute("shortSign")} - Estimated Arrival: #{arriveString} (Scheduled at #{scheduled.strftime("%H:%M")})\n"
-      end
-  end
+def parse_time(time)
+  seconds   = time % 60
+  minutes   = ((time - seconds) % 3600) / 60
+  hours     = (time - seconds) / 3600
+
+  return { :seconds => seconds, :minutes => minutes, :hours => hours }
 end
