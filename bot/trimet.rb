de1e94a2abd1c799938b310d221b46b9749c32b2
Matthaus Owens <matthaus@puppetlabs.com>
2013-01-22 21:43:23 -0800

    Added TrimetBot module. Included in main bot logic.

--- bot/trimet.rb
+++ bot/trimet.rb
@@ -16,6 +16,7 @@ else
 end
 
 bot = Cinch::Bot.new do
+
   configure do |c|
     c.server    = CONFIG[:server]
     c.port      = CONFIG[:port]
@@ -25,81 +26,18 @@ bot = Cinch::Bot.new do
   end
 
   helpers do
-    # This method assumes everything will go ok, it's not the best method
-    # of doing this *by far* and is simply a helper method to show how it
-    # can be done.. it works!
-    def trimet(query)
-      @apikey = CONFIG[:api_key]
-      query = quey.split(",")
-      stopID = qury[0].strip
-      lineNum = (uery[1] == nil ? nil : query[1].strip)
-      count = 0
-
-      returnString = String.new
-      url = "http://developer.trimet.org/ws/V1/arrivals?locIDs=#{stopID}&appID=#{@apikey}"
-      results = Hash.new
-
-      doc = Nokogiri::XML::Reader(open(url))
-      doc.each do | node |
-        case node.name
-          when "location"
-            if (node.depth == 1)
-              returnString << "Stop ID: #{stopID} - #{node.attribute("desc")}\n"
-            end
-          when "arrival"
-            count += 1
-            if (lineNum == nil or node.attribute("route") == lineNum)
-              block = node.attribute("block")
-              curTime = Time.now.to_i
-              arrival = node.attribute("estimated")
-              arrival = Time.at(arrival[0..arrival.length-4].to_i)
-              arriveDiff = (arrival.to_i - curTime)
-              if (node.attribute("status") == "estimated")
-                if (arriveDiff > 3600)
-                  arriveString = "#{arriveDiff/3600} hour(s), #{(arriveDiff%3600)/60} minutes, #{(arriveDiff%3600)%60} seconds"
-                else
-                  arriveString = "#{arriveDiff/60} minutes, #{arriveDiff%60} seconds"
-                end
-              else
-                arriveString = "<no estimate available>"
-              end
-              scheduled = node.attribute("scheduled")
-              scheduled = Time.at(scheduled[0..scheduled.length-4].to_i)
-              results[block] = "#{node.attribute("shortSign")} - Estimated Arrival: #{arriveString} (Scheduled at #{scheduled.strftime("%H:%M")})\n"
-            end
-        end
-      end
-
-      if count > 0
-        results.each do |key, value|
-          returnString << "#{value}"
-        end
-      else
-        returnString << "No results for that query.\n"
-      end
-
-      return returnString
-    end
+    load 'trimetbot.rb'
+    include TrimetBot
   end
 
-  on :message, /^!trimet (.+)/ do |m, term|
-    if term == "help"
-      m.reply("Usage: !trimet <stopID> for transit tracker results for stopID.")
+  on :message, /^(!trimet|TrimetBot:)\s*(\d+|help)[,\s]*(\d+)?/ do |m, trash, term, route|
+    if term.match(/\d+/)
+      m.reply(trimet(term, route))
     else
-      m.reply(trimet(term) || "No results found", true)
-    end
-  end
-
-  on :message, /^(!trimet|Trimet).*(help)/ do |m, target, term|
-    if term == "help"
       m.reply("Usage: !trimet <stopID> for transit tracker results for stopID.")
+      m.reply("       !trimet <stopID>, <line> for tracker results for the line at that stopID.")
     end
   end
-
 end
 
 bot.start
-
-# injekt> !urban cinch
-# MrCinch> injekt: describing an action that's extremely easy.
-
---
+++ bot/trimetbot.rb
@@ -0,0 +1,61 @@
+module TrimetBot
+  def trimet(stopID, route)
+    doc = Nokogiri::XML(open("http://developer.trimet.org/ws/V1/arrivals?locIDs=#{stopID}&appID=#{CONFIG[:api_key]}"))
+    begin
+      parse_results(doc, stopID, route)
+    rescue Exception => e
+      return e.message
+    end
+  end
+
+  def parse_results(doc, stopID, route)
+    unless (t_loc = doc.xpath('//xmlns:location')).empty?
+      location = t_loc.attribute("desc").value
+    end
+
+    if route
+      arrivals = doc.xpath("//xmlns:arrival[@route=#{route}]")
+    else
+      arrivals = doc.xpath('//xmlns:arrival')
+    end
+
+    unless (t_error = doc.xpath('//xmlns:errorMessage')).empty?
+      error = t_error.inner_text
+      raise error
+    end
+
+    results = []
+    results << "Stop ID: #{stopID} - #{location}"
+
+    arrivals.each do |node|
+      puts node.name
+      if node.attributes.include?("estimated")
+        estimated = node.attribute("estimated").value.to_i/1000
+        estimate = parse_time(estimated - Time.now.to_i)
+
+        if estimate[:hours].zero?
+          arrival = "#{estimate[:minutes]} minutes, #{estimate[:seconds]} seconds"
+        else
+          arrival = "#{estimate[:hours]} hour(s), #{estimate[:minutes]} minutes, #{estimate[:seconds]} seconds"
+        end
+      end
+
+      if node.attributes.include?("scheduled")
+        scheduled = Time.at(node.attribute("scheduled").value.to_i/1000)
+        arrival = "<no estimate available>" if arrival.nil?
+      end
+
+      results << "#{node.attribute("shortSign")} - Estimated Arrival: #{arrival} (Scheduled at #{scheduled.strftime("%H:%M")})"
+    end
+
+    results.join("\n")
+  end
+
+  def parse_time(time)
+    seconds   = time % 60
+    minutes   = ((time - seconds) % 3600) / 60
+    hours     = (time - seconds) / 3600
+
+    return { :seconds => seconds, :minutes => minutes, :hours => hours }
+  end
+end
