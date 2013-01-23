c7f34f0e3734bfb9a161a5884459959b6e0315f4
Matthaus Owens <matthaus@puppetlabs.com>
2013-01-22 21:46:23 -0800

    Add fail case for arrivals.

--- bot/trimetbot.rb
+++ bot/trimetbot.rb
@@ -27,25 +27,29 @@ module TrimetBot
     results = []
     results << "Stop ID: #{stopID} - #{location}"
 
-    arrivals.each do |node|
-      puts node.name
-      if node.attributes.include?("estimated")
-        estimated = node.attribute("estimated").value.to_i/1000
-        estimate = parse_time(estimated - Time.now.to_i)
+    if arrivals
+      arrivals.each do |node|
+        puts node.name
+        if node.attributes.include?("estimated")
+          estimated = node.attribute("estimated").value.to_i/1000
+          estimate = parse_time(estimated - Time.now.to_i)
 
-        if estimate[:hours].zero?
-          arrival = "#{estimate[:minutes]} minutes, #{estimate[:seconds]} seconds"
-        else
-          arrival = "#{estimate[:hours]} hour(s), #{estimate[:minutes]} minutes, #{estimate[:seconds]} seconds"
+          if estimate[:hours].zero?
+            arrival = "#{estimate[:minutes]} minutes, #{estimate[:seconds]} seconds"
+          else
+            arrival = "#{estimate[:hours]} hour(s), #{estimate[:minutes]} minutes, #{estimate[:seconds]} seconds"
+          end
         end
-      end
 
-      if node.attributes.include?("scheduled")
-        scheduled = Time.at(node.attribute("scheduled").value.to_i/1000)
-        arrival = "<no estimate available>" if arrival.nil?
-      end
+        if node.attributes.include?("scheduled")
+          scheduled = Time.at(node.attribute("scheduled").value.to_i/1000)
+          arrival = "<no estimate available>" if arrival.nil?
+        end
 
-      results << "#{node.attribute("shortSign")} - Estimated Arrival: #{arrival} (Scheduled at #{scheduled.strftime("%H:%M")})"
+        results << "#{node.attribute("shortSign")} - Estimated Arrival: #{arrival} (Scheduled at #{scheduled.strftime("%H:%M")})"
+      end
+    else
+      results << "No arrivals returned for stop ID: #{stopID}."
     end
 
     results.join("\n")
